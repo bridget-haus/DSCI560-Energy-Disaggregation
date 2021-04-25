2a72fa6202b66c4705d4f2c0cf5e49b995585018
Youhyon (Lena) Kim <youhyonk@usc.edu>
2021-04-24 20:22:42 -0700

    delete unnecessary data

--- Dashboard/dashboard/src/charts/neighbors/Neighbors.js
+++ Dashboard/dashboard/src/charts/neighbors/Neighbors.js
@@ -1,16 +1,8 @@
 import BarChart from "./BarChart";
-import neighborData from './data-neighbor.json'
+import neighborData from '../data/nn_appliances.json'
 import './Neighbors.css'
-// import {BarChart} from 'react-charts-d3'
 
 function NeighborChart () {
-
-    // const data = neighborData;
-    const data = [
-        { key: 'Main', values: [ { x: 'A', y: 23 }, { x: 'B', y: 8 } ] },
-        { key: 'Lighting', values: [ { x: 'A', y: 15 }, { x: 'B', y: 37 } ] },
-        { key: 'Washer/Dryer', values: [ { x: 'A', y: 36 }, { x: 'B', y: 9 } ] },
-    ];
 
     return (
         <BarChart
