6fbca6932554162a440da2a4b2483c4be7ddfe6f
Youhyon (Lena) Kim <youhyonk@usc.edu>
2021-04-25 22:50:20 -0700

    Set usage window to 30 minutes

--- Dashboard/data-manipulation/clean-save-result.py
+++ Dashboard/data-manipulation/clean-save-result.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pandas as pd
 import pickle5 as pkl
 from pathlib import Path
 from collections import defaultdict
@@ -35,32 +36,36 @@ for file in os.listdir(result_dir):
         with open(file_dir, 'rb') as f:
 
             df = pkl.load(f)
-            df['timestamp'] = df['timestamp'].astype(str)
+            df['timestamp'] = df['timestamp'].astype('datetime64[ns]')
             total_usage = df['prediction'].sum()
             appliance_dict['usage'] = total_usage
-            inner_dict['values'] = df.to_dict(orient='records')
+
+            df_30 = df.groupby([pd.Grouper(key='timestamp', freq='30min')]).sum()
+            df_30 = df_30.reset_index()
+            df_30['timestamp'] = df_30['timestamp'].astype(str)
+            inner_dict['values'] = df_30.to_dict(orient='records')
 
         appliances.append(appliance_dict)
         houses[house].append(inner_dict)
 
-    if model == 'svm':
+    # if model == 'svm':
+    #
+    #     appliance = file[19:-4]
+    #
+    #     with open(file_dir, 'rb') as f:
+    #
+    #         appliances[appliance] = pkl.load(f)
 
-        appliance = file[19:-4]
 
-        with open(file_dir, 'rb') as f:
-
-            appliances[appliance] = pkl.load(f)
-
-
-if not os.isfile(output_file_nn_houses):
+if not os.path.isfile(output_file_nn_houses):
     with open(output_file_nn_houses, 'w') as f:
         json.dump(houses, f)
 
-if not os.isfile(output_file_nn_appliances):
+if not os.path.isfile(output_file_nn_appliances):
     with open(output_file_nn_appliances, 'w') as f:
         json.dump(appliances, f)
 
-if not os.isfile(output_file_svm):
+if not os.path.isfile(output_file_svm):
     with open(output_file_svm, 'w') as f:
         json.dump(houses, f)
 
