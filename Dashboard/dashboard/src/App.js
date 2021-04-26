ceede6881a9b7c543a03571ede5484effa80ffd1
Youhyon (Lena) Kim <youhyonk@usc.edu>
2021-04-25 22:48:33 -0700

    Minor design updates

--- Dashboard/dashboard/src/App.js
+++ Dashboard/dashboard/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h3>
           My Energy
         </h3>
